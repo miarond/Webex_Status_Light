77f0aa38ea39d280d5aa7079d2e2d283afd2298c
Aron Donaldson <miarond@gmail.com>
2025-06-17 16:50:41 -0500

    Fixing sed again

--- setup.sh
+++ setup.sh
@@ -24,10 +24,10 @@ echo "Environment=PERSON="$person >> mycredentials.txt
 # wget -O rgbtest.py https://raw.githubusercontent.com/miarond/Webex_Status_Light/main/rgbtest.py
 
 # Update service file with creds using 'sed' to find & replace 'foo' and 'bar' placeholders with user's credentials.
-sed -ie 's@foo@'"$accessToken"'@' webexapp.service
-sed -ie 's@bar@'"$person"'@' webexapp.service
-sed -ie 's@<user>@'"$USER"'@' webexapp.service
-sed -ie 's@<replace>@'"$PWD"'@' webexapp.service
+sed -ie 's@foo@'"$accessToken"'@g' webexapp.service
+sed -ie 's@bar@'"$person"'@g' webexapp.service
+sed -ie 's@<user>@'"$USER"'@g' webexapp.service
+sed -ie 's@<replace>@'"$PWD"'@g' webexapp.service
 
 # Copy the service's unit file out to systemd, then register app as a service
 sudo cp webexapp.service /etc/systemd/system
